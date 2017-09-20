751417e195c6d534f9cb9ca308d8596d0bfc6422
lmk123 <me@limingkai.cn>
2017-09-20 16:03:18 +0800

    使用 Node.js 脚本替换 es6 import 语法

---
+++ build/replace-es6-import.js
@@ -0,0 +1,8 @@
+const fs = require('fs')
+const path = require('path')
+
+const file = path.resolve(__dirname, '../src/api/youdao.ts')
+
+const str = fs.readFileSync(file, 'utf8')
+
+fs.writeFileSync(file, str.replace('import md5 from \'blueimp-md5\'', 'import md5 = require(\'blueimp-md5\')'))
