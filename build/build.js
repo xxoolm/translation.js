4b26110f332835ec507d290a5475d81474eaae49
lmk123 <me@limingkai.cn>
2017-09-25 14:17:10 +0800

    改进浏览器端打包脚本

--- build/build.js
+++ build/build.js
@@ -1,32 +1,42 @@
 const path = require('path')
 const fs = require('fs-extra')
-const config = require('./config')
+const typescript = require('rollup-plugin-typescript2')
+const pkg = require('../package.json')
+const rollup = require('rollup')
+const uglifyJS = require('uglify-js')
 
 // 清空输出目录
 fs.emptyDirSync(path.resolve(__dirname, '../dist'))
 
-// 编译 js
-const rollup = require('rollup')
-const uglifyJS = require('uglify-js')
+const md5Adapter = path.resolve(__dirname, '../src/adapters/md5/node.ts')
 
 rollup.rollup({
-  input: config.input,
-  external: config.external,
+  input: path.resolve(__dirname, '../src/index.ts'),
+  external: [
+    'superagent',
+    md5Adapter
+  ],
   plugins: [
-    config.tp
+    typescript()
   ]
 }).then(bundle => {
   // 输出 umd 格式
   bundle.generate({
     format: 'iife',
-    name: config.name,
+    name: 'tjs',
     globals: {
       superagent: 'superagent',
-      [path.resolve(__dirname, '../src/adapters/md5/node.ts')]: 'md5'
+      [md5Adapter]: 'md5'
     },
-    banner: config.banner
+    banner: [
+      '/*!',
+      ' * translator.js v' + pkg.version,
+      ' * https://github.com/Selection-Translator/translation.js',
+      ' * Released under the MIT License.',
+      ' */'
+    ].join('\n')
   }).then(({ code }) => {
-    fs.writeFile(config.umdOutputPath, code)
-    fs.writeFile(config.umdMinOutputPath, uglifyJS.minify(code, { output: { comments: /^!/ } }).code)
+    fs.writeFile(path.resolve(__dirname, '../dist/translator.js'), code)
+    fs.writeFile(path.resolve(__dirname, '../dist/translator.min.js'), uglifyJS.minify(code, { output: { comments: /translator\.js/ } }).code)
   })
 })
