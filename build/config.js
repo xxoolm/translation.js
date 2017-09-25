6ee4eba87a8b544013345a1d914c5244d3eb18eb
lmk123 <me@limingkai.cn>
2017-09-25 11:26:46 +0800

    改为使用 tsc 将源码编译成 CommonJS，并使用 rollup 打包浏览器版本

--- build/config.js
+++ build/config.js
@@ -5,12 +5,8 @@ const pkg = require('../package.json')
 module.exports = {
   input: path.resolve(__dirname, '../src/index.ts'),
   name: 'tjs',
-  tp: typescript({
-    useTsconfigDeclarationDir: true
-  }),
+  tp: typescript(),
   external: Object.keys(pkg.dependencies),
-  esOutputPath: path.resolve(__dirname, '../dist/translator.esm.js'),
-  cjsOutputPath: path.resolve(__dirname, '../dist/translator.common.js'),
   umdOutputPath: path.resolve(__dirname, '../dist/translator.js'),
   umdMinOutputPath: path.resolve(__dirname, '../dist/translator.min.js'),
   banner: [
