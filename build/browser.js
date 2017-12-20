312b25c67e3db0719b7960bc5079f11f8f323662
lmk123 <me@limingkai.cn>
2017-12-20 11:13:45 +0800

    修复 md5 函数在浏览器中使用时不兼容 es6 模块的问题

--- build/browser.js
+++ build/browser.js
@@ -9,42 +9,60 @@ const uglifyJS = require('uglify-js')
 fs.emptyDirSync(path.resolve(__dirname, '../dist'))
 
 const xhrAdapter = path.resolve(__dirname, '../src/adapters/http/xhr.ts')
+const md5Adapter = path.resolve(__dirname, '../src/adapters/md5/browser.ts')
 
-rollup.rollup({
-  input: path.resolve(__dirname, '../src/index.ts'),
-  external: ['blueimp-md5'],
-  plugins: [
-    {
-      resolveId (importee) {
-        // 将 http adapter 替换成 xhr adapter
-        if (importee.endsWith('/adapters/http/node')) {
-          return xhrAdapter
+const orgFile = fs.readFileSync(md5Adapter, 'utf8')
+fs.writeFileSync(md5Adapter, orgFile.replace("import bluemd5 = require('blueimp-md5')", "import bluemd5 from 'blueimp-md5'"))
+
+rollup
+  .rollup({
+    input: path.resolve(__dirname, '../src/index.ts'),
+    external: ['blueimp-md5'],
+    plugins: [
+      {
+        resolveId(importee) {
+          // 将 http adapter 替换成 xhr adapter
+          if (importee.endsWith('/adapters/http/node')) {
+            return xhrAdapter
+          }
+          // 将 Node.js 中的 md5 实现替换成浏览器端的 md5 实现
+          if (importee.endsWith('/adapters/md5/node')) {
+            return md5Adapter
+          }
         }
-        // 将 Node.js 中的 md5 实现替换成浏览器端的 md5 实现
-        if (importee.endsWith('/adapters/md5/node')) {
-          return 'blueimp-md5'
+      },
+      typescript({
+        tsconfigOverride: {
+          compilerOptions: {
+            allowSyntheticDefaultImports: true
+          }
         }
-      }
-    },
-    typescript()
-  ]
-}).then(bundle => {
-  // 输出 umd 格式
-  bundle.generate({
-    format: 'umd',
-    name: 'tjs',
-    globals: {
-      'blueimp-md5': 'md5'
-    },
-    banner: [
-      '/*!',
-      ' * translator.js v' + pkg.version,
-      ' * https://github.com/Selection-Translator/translation.js',
-      ' * Released under the MIT License.',
-      ' */'
-    ].join('\n')
-  }).then(({ code }) => {
-    fs.writeFile(path.resolve(__dirname, '../dist/translator.js'), code)
-    fs.writeFile(path.resolve(__dirname, '../dist/translator.min.js'), uglifyJS.minify(code, { output: { comments: /translator\.js/ } }).code)
+      })
+    ]
   })
-})
+  .then(bundle => {
+    // 输出 umd 格式
+    bundle
+      .generate({
+        format: 'umd',
+        name: 'tjs',
+        globals: {
+          'blueimp-md5': 'md5'
+        },
+        banner: [
+          '/*!',
+          ' * translator.js v' + pkg.version,
+          ' * https://github.com/Selection-Translator/translation.js',
+          ' * Released under the MIT License.',
+          ' */'
+        ].join('\n')
+      })
+      .then(({ code }) => {
+        fs.writeFile(md5Adapter, orgFile)
+        fs.writeFile(path.resolve(__dirname, '../dist/translator.js'), code)
+        fs.writeFile(
+          path.resolve(__dirname, '../dist/translator.min.js'),
+          uglifyJS.minify(code, { output: { comments: /translator\.js/ } }).code
+        )
+      })
+  })
---
+++ src/adapters/md5/browser.ts
@@ -0,0 +1,3 @@
+// blueimp-md5 是一个 CommonJS 模块，为了跟 ./node.ts 保持一致，这里将它转换为 ES6 模块
+import bluemd5 = require('blueimp-md5')
+export default bluemd5
